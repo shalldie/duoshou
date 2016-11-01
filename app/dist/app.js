5b1acfe3809462e4c18cf2b5a995a077d671e88d
谢爽 <394229785@qq.com>
2016-11-01 16:00:01 +0800

    完成主要逻辑

--- app/dist/app.js
+++ app/dist/app.js
@@ -112,20 +112,24 @@
 	// 最外部容器swiper
 	var mainSwiper = new Swiper('#main_wrap', {
 	    effect: 'fade',
-	    noSwiping: true
-	    // direction: 'vertical',
-	    // loop: true,
-
-	    // // 如果需要分页器
-	    // pagination: '.swiper-pagination',
-
-	    // // 如果需要前进后退按钮
-	    // nextButton: '.swiper-button-next',
-	    // prevButton: '.swiper-button-prev',
-
-	    // // 如果需要滚动条
-	    // scrollbar: '.swiper-scrollbar',
+	    // noSwiping: true
+	    "": ""
 	});
+
+	// 结果页面上下滑动
+	var resultSwiper = new Swiper('#result_wrap', {
+	    direction: 'vertical',
+	    "": ""
+	});
+
+	$(".btn-more").click(function () {
+	    resultSwiper.slideTo(1);
+	});
+
+	$(".btn-ag").click(function () {
+	    newGame();
+	});
+
 	__webpack_require__(9)().done(function () {
 	    setTimeout(function () {
 	        mainSwiper.slideTo(1);
@@ -146,8 +150,44 @@
 	// 第二屏，点击按钮跳转到第三屏
 	$(".start-btn").click(function () {
 	    mainSwiper.slideTo(2);
-	    game = new Game();
+	    newGame();
 	});
+
+	function newGame() {
+	    mainSwiper.slideTo(2);
+	    game = new Game();
+	    game.end.add(function (score) {  // 游戏结束
+	        mainSwiper.slideTo(3);
+	        showResult(score);
+	    });
+	}
+
+
+	// 结果页面
+
+	function showResult(score) {
+	    var level = 4;
+	    if (score > 12) {
+	        level = 1;
+	    } else if (score > 10) {
+	        level = 2;
+	    } else if (score > 8) {
+	        level = 3;
+	    } else {
+	        level = 4;
+	    }
+
+	    var content = [
+	        '<h2>骨灰级剁手党</h2><p>总在深夜想起<br/>诗和远方<br/>还有钱包空荡荡</p>',
+	        '<h2>重度剁手党患者</h2><p>买买买时指点江山<br/>激扬文字<br/>却总在深夜听钱哭的声音</p>',
+	        '<h2>中度剁手党患者</h2><p>买买买时残留<br/>一丝“人性”<br/>尚能顾虑到钱包的感受</p>',
+	        '<h2>专业伪剁手党</h2><p>梦想成为一枚叱咤江湖<br/>的剁手党老大<br/>奈何金钱总是投奔他方</p>'
+	    ][level - 1];
+
+	    var src = `img/result-imgs0${level}.png`;
+	    $(".result-img").attr("src", src);
+	    $(".result-content").html(content);
+	}
 
 /***/ },
 /* 7 */
@@ -160,6 +200,10 @@
 	class Game {
 	    constructor() {
 	        this.init();
+	        this.score = 0;
+	        this.timeLeft = 10;
+	        this.render();
+	        this.end = $.Callbacks();
 	    }
 
 	    /**
@@ -169,9 +213,124 @@
 	     * @memberOf Game
 	     */
 	    init() {
+	        var self = this;
+	        this.bindEvents();
+	        this.render();
 	        gameLoading(function () {
-
+	            self.start();
+	            self.tick();
 	        });
+	    }
+
+	    /**
+	     * 事件绑定
+	     * 
+	     * 
+	     * @memberOf Game
+	     */
+	    bindEvents() {
+	        var wrap = $(".game-wrap").off();
+	        var self = this;
+
+	        wrap.click(function () {
+	            $("#music_duo")[0].play();
+	        });
+
+	        wrap.on("click", ".duo", function () { // 点击到无效区域
+	            var self = $(this);
+	            if (self.data("hasClick")) return;
+
+	            self.data("hasClick", true);
+	            self.attr("class", "duo duo—styleC");
+	            clearTimeout(self.data("timer"));
+	            setTimeout(function () {
+	                self.remove();
+	            }, 1000);
+	        });
+
+	        wrap.on("click", ".duo-p", function () { // 点击到有效区域
+	            var eleP = $(this).parent();
+	            if (eleP.data("hasClick")) return;
+	            eleP.data("hasClick", true);
+
+	            eleP.attr("class", "duo duo—styleB");
+	            self.score++;
+	            self.render();
+
+	            clearTimeout(eleP.data("timer"));
+	            setTimeout(function () {
+	                eleP.remove();
+	            }, 1000);
+
+	            $("#music_success")[0].play();
+	            return false;
+	        });
+
+	    }
+
+	    newItem() {
+	        var content = `
+	        <div class="duo duo—styleA">
+	            <a href="javascript:void(0)" class="duo-p"></a>
+	            <div class="duo-knife"></div>
+	        </div>
+	        `;
+	        var ele = $(content).css({
+	            "left": ~~(Math.random() * 80) + "%",
+	            "top": ~~(Math.random() * 80) + "%",
+	            "transform": `scale(${1 + +Math.random().toFixed(2)})`
+	        });
+	        $(".game-box").append(ele);
+	        var timer = setTimeout(function () {
+	            ele.remove();
+	        }, 1000);
+	        ele.data("timer", timer);
+	    }
+
+	    /**
+	     * 开始游戏
+	     * 
+	     * @param {any} delay
+	     * @returns
+	     * 
+	     * @memberOf Game
+	     */
+	    start(delay) {
+	        if (this.timeLeft <= 0) {
+	            return;
+	        }
+
+	        var self = this;
+	        delay = delay || 1000;
+	        setTimeout(function () {
+	            self.newItem();
+	            self.start(delay - 40);
+	        }, delay)
+	    }
+
+	    tick() {
+	        var self = this;
+	        var timer = setInterval(function () {
+	            self.timeLeft--;
+	            self.render();
+	            if (self.timeLeft <= 0) {
+	                clearInterval(timer);
+	                setTimeout(function () {
+	                    self.end.fire(self.score);
+	                }, 1100);
+	            }
+	        }, 1000);
+	    }
+
+	    /**
+	     * 将积分和结果渲染到页面
+	     * 
+	     * 
+	     * @memberOf Game
+	     */
+	    render() {
+	        $(".game-score").html(this.score);
+	        $(".game-time").html(this.timeLeft);
 	    }
 	}
 
--- app/src/app/game.js
+++ app/src/app/game.js
@@ -5,6 +5,10 @@ var gameLoading = require('./gameLoading');
 class Game {
     constructor() {
         this.init();
+        this.score = 0;
+        this.timeLeft = 10;
+        this.render();
+        this.end = $.Callbacks();
     }
 
     /**
@@ -14,9 +18,124 @@ class Game {
      * @memberOf Game
      */
     init() {
+        var self = this;
+        this.bindEvents();
+        this.render();
         gameLoading(function () {
-
+            self.start();
+            self.tick();
         });
+    }
+
+    /**
+     * 事件绑定
+     * 
+     * 
+     * @memberOf Game
+     */
+    bindEvents() {
+        var wrap = $(".game-wrap").off();
+        var self = this;
+
+        wrap.click(function () {
+            $("#music_duo")[0].play();
+        });
+
+        wrap.on("click", ".duo", function () { // 点击到无效区域
+            var self = $(this);
+            if (self.data("hasClick")) return;
+
+            self.data("hasClick", true);
+            self.attr("class", "duo duo—styleC");
+            clearTimeout(self.data("timer"));
+            setTimeout(function () {
+                self.remove();
+            }, 1000);
+        });
+
+        wrap.on("click", ".duo-p", function () { // 点击到有效区域
+            var eleP = $(this).parent();
+            if (eleP.data("hasClick")) return;
+            eleP.data("hasClick", true);
+
+            eleP.attr("class", "duo duo—styleB");
+            self.score++;
+            self.render();
+
+            clearTimeout(eleP.data("timer"));
+            setTimeout(function () {
+                eleP.remove();
+            }, 1000);
+
+            $("#music_success")[0].play();
+            return false;
+        });
+
+    }
+
+    newItem() {
+        var content = `
+        <div class="duo duo—styleA">
+            <a href="javascript:void(0)" class="duo-p"></a>
+            <div class="duo-knife"></div>
+        </div>
+        `;
+        var ele = $(content).css({
+            "left": ~~(Math.random() * 80) + "%",
+            "top": ~~(Math.random() * 80) + "%",
+            "transform": `scale(${1 + +Math.random().toFixed(2)})`
+        });
+        $(".game-box").append(ele);
+        var timer = setTimeout(function () {
+            ele.remove();
+        }, 1000);
+        ele.data("timer", timer);
+    }
+
+    /**
+     * 开始游戏
+     * 
+     * @param {any} delay
+     * @returns
+     * 
+     * @memberOf Game
+     */
+    start(delay) {
+        if (this.timeLeft <= 0) {
+            return;
+        }
+
+        var self = this;
+        delay = delay || 1000;
+        setTimeout(function () {
+            self.newItem();
+            self.start(delay - 40);
+        }, delay)
+    }
+
+    tick() {
+        var self = this;
+        var timer = setInterval(function () {
+            self.timeLeft--;
+            self.render();
+            if (self.timeLeft <= 0) {
+                clearInterval(timer);
+                setTimeout(function () {
+                    self.end.fire(self.score);
+                }, 1100);
+            }
+        }, 1000);
+    }
+
+    /**
+     * 将积分和结果渲染到页面
+     * 
+     * 
+     * @memberOf Game
+     */
+    render() {
+        $(".game-score").html(this.score);
+        $(".game-time").html(this.timeLeft);
     }
 }
 
